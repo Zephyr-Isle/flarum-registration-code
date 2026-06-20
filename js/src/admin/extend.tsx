b02b5a4d479734fd23fb9df606f848db022070d4
Zephyr-Isle <zephyr_isle@outlook.com>
2026-06-20 12:46:37 +0800

    Test with simple boolean setting only
    - Remove JSX callback for complex component
    - Test if basic boolean setting displays
    - This will help identify if the issue is with the setting registration or the JSX callback

--- js/src/admin/extend.tsx
+++ js/src/admin/extend.tsx
@@ -1,6 +1,5 @@
 import Extend from 'flarum/common/extenders';
 import app from 'flarum/admin/app';
-import RegistrationCodeManager from './components/RegistrationCodeManager';
 
 export default [
   new Extend.Admin()
@@ -9,12 +8,4 @@ export default [
       label: app.translator.trans('zephyrisle-registration-code.admin.settings.enabled_label', {}, true),
       type: 'boolean',
     }), 30)
-    .setting(() => function () {
-      return (
-        <div className="Form-group">
-          <h3>{app.translator.trans('zephyrisle-registration-code.admin.manager.heading')}</h3>
-          <RegistrationCodeManager />
-        </div>
-      );
-    }, 20)
 ];
