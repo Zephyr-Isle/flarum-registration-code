882924af404c4686dc1e526cb46859a0244857e6
Zephyr-Isle <zephyr_isle@outlook.com>
2026-06-20 12:09:58 +0800

    Remove .setting() call from extend.ts
    - When using .page() with custom page, .setting() won't appear
    - Settings should be rendered via buildSettingComponent in custom page only

--- js/src/admin/extend.ts
+++ js/src/admin/extend.ts
@@ -1,13 +1,7 @@
 import Extend from 'flarum/common/extenders';
-import app from 'flarum/admin/app';
 import RegistrationCodePage from './components/RegistrationCodePage';
 
 export default [
   new Extend.Admin()
     .page(RegistrationCodePage)
-    .setting(() => ({
-      setting: 'zephyrisle-registration-code.enabled',
-      label: app.translator.trans('zephyrisle-registration-code.admin.settings.enabled_label', {}, true),
-      type: 'boolean',
-    })),
 ];
