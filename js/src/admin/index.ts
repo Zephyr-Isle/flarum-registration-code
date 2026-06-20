2e0214efe037ba4ddfb51a3a5536dde37b010ab0
Zephyr-Isle <zephyr_isle@outlook.com>
2026-06-20 13:09:01 +0800

    Revert to old Flarum API for admin settings
    - Use app.initializers.add() with extend() function
    - Remove extend.tsx file (new API approach)
    - This approach was used in original Flarum v1 code
    - Test if old API works better than new extend API

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,1 +1,12 @@
-export { default as extend } from './extend';
+import app from 'flarum/admin/app';
+import { extend } from 'flarum/common/extend';
+
+app.initializers.add('zephyrisle/flarum-registration-code', () => {
+  extend(app.extensionData, 'settings', (settings) => {
+    settings.addSetting({
+      setting: 'zephyrisle-registration-code.enabled',
+      label: app.translator.trans('zephyrisle-registration-code.admin.settings.enabled_label', {}, true),
+      type: 'boolean',
+    });
+  });
+});
